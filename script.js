5074da7d06dfc467cfe33effb310ded6214c9282
theMungai <mungaithesimon@gmail.com>
2024-11-26 19:02:08 +0300

    Styled the landing page

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 subscribeButton.addEventListener("click", () => {
     if(userInput.value !== "" && userInput.type === "email" && emailPattern.test(userInput.value) ){
-        message.textContent = `Thanks for Subscring. 
-        A confirmation message has been sent to ${userInput.value}`
-        message.style.color = ""
+        message.textContent = `Thanks for Subscribing.\nA confirmation message has been sent to ${userInput.value}`
+        message.style.color = "hsl(322, 100%, 66%)"
+        message.style.width = "55%"
+    }
+
+    else{
+        message.textContent = "Check your email please";
+        message.style.color = "hsl(0, 100%, 63%)"
+        userInput.style.border = "2px solid hsl(0, 100%, 63%)"
     }
 })
